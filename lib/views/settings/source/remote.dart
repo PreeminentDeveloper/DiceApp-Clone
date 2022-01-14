cbdf41a9d5fca57f3d34f8388b5f7e36cde0bb6f
mopah emma <mopahemma@gmail.com>
2022-01-15 00:43:46 +0100

    impl block user list

--- lib/views/settings/source/remote.dart
+++ lib/views/settings/source/remote.dart
@@ -9,7 +9,31 @@ class SetUpService {
   SetUpService({required DiceGraphQLClient networkService})
       : _graphQLClient = networkService;
 
-  Future<IgnoreUsersResponse> listIgnoreUserd(
+  Future<IgnoreUsersResponse> listIgnoreUser(
+      {required int pageNumber,
+      int perPage = 20,
+      required String search,
+      required String userID}) async {
+    try {
+      final _result = await _graphQLClient.client.query(
+        QueryOptions(
+            document: gql(SetUpGql.listIgnoredUsers),
+            variables: {
+              "pageNo": pageNumber,
+              "perPage": perPage,
+              "search": search,
+              "userId": userID
+            },
+            fetchPolicy: FetchPolicy.networkOnly),
+      );
+      return IgnoreUsersResponse.fromJson(_result.data!);
+    } catch (exception) {
+      logger.e(exception);
+      rethrow;
+    }
+  }
+
+  Future<IgnoreUsersResponse> blockIgnoreUser(
       {required int pageNumber,
       int perPage = 20,
       required String search,
