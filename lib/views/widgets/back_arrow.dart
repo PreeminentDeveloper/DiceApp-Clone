1c65d0a4902767013b1dabe3a97a9e877807f3dd
JayCode <vsquare396@gmail.com>
2022-02-14 12:33:45 +0100

    Updated

--- lib/views/widgets/back_arrow.dart
+++ lib/views/widgets/back_arrow.dart
@@ -1,4 +1,3 @@
-
 import 'package:dice_app/core/navigation/page_router.dart';
 import 'package:dice_app/core/util/size_config.dart';
 import 'package:flutter/material.dart';
@@ -6,13 +5,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class BackArrow extends StatelessWidget {
+  final Function()? onBackTapped;
+
+  BackArrow({this.onBackTapped});
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Align(
       alignment: Alignment.topLeft,
       child: GestureDetector(
-        onTap: () => PageRouter.goBack(context),
+        onTap: onBackTapped ?? () => PageRouter.goBack(context),
         child: Container(
           margin: EdgeInsets.only(
               left: SizeConfig.sizeXXL!, bottom: SizeConfig.sizeXL!),
